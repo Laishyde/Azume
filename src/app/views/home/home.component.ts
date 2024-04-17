ca9a480405e83f93aadcd7dddc6c6fd9d3cbc798
syaoranea <eduardo_rocha87@hotmail.com>
2024-04-17 19:43:30 -0300

    feat(services): create section

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
+import { SectionServiceComponent } from '../../shared/components/section-service/section-service.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, SectionServiceComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
