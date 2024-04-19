b297b72a6ef6993eab27d3831911adb3f35bd074
syaoranea <eduardo_rocha87@hotmail.com>
2024-04-19 20:11:52 -0300

    feat(clientes): create cliente

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { SectionServiceComponent } from '../../shared/components/section-service/section-service.component';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
+import { DepoimentosComponent } from '../../shared/components/depoimentos/depoimentos.component';
+import { ClientesComponent } from '../../shared/components/clientes/clientes.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, SectionServiceComponent, FooterComponent],
+  imports: [HeaderComponent, SectionServiceComponent, FooterComponent, DepoimentosComponent, ClientesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
