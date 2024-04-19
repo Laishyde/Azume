b9c011cb69de6b7a90db4ca83e79e841e37b6b9c
Laishyde <laiscristina.animamundi@gmail.com>
2024-04-19 18:30:27 -0300

    feito alterações de component team

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { SectionServiceComponent } from '../../shared/components/section-service/section-service.component';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
+import { TeamComponent } from "../../shared/components/team/team.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [HeaderComponent, SectionServiceComponent, FooterComponent],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss',
+    imports: [HeaderComponent, SectionServiceComponent, FooterComponent, TeamComponent]
 })
 export class HomeComponent {
 
